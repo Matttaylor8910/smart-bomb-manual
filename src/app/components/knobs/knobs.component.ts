0c0b1421c8a99fc1f0c2c806b48926fd45f06ab2
Brik Royster <elkemore@gmail.com>
2020-11-01 20:06:02 -0700

    implements nobs

--- src/app/components/knobs/knobs.component.ts
+++ src/app/components/knobs/knobs.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+
+const KNOB_ANSWERS = {
+  [[false, false, true, true, true, true].toString()]: 'Up',
+  [[false, true, false, false, true, true].toString()]: 'Up',
+  [[false, true, true, true, true, true].toString()]: 'Down',
+  [[true, false, true, false, true, false].toString()]: 'Down',
+  [[false, false, false, true, false, false].toString()]: 'Left',
+  [[false, false, false, false, false, false].toString()]: 'Left',
+  [[true, false, true, true, true, true].toString()]: 'Right',
+};
 
 @Component({
   selector: 'app-knobs',
   templateUrl: './knobs.component.html',
   styleUrls: ['./knobs.component.scss'],
 })
-export class KnobsComponent implements OnInit {
+export class KnobsComponent {
+  knobs = [false, false, false, false, false, false];
 
-  constructor() { }
+  get answer(): string {
+    return KNOB_ANSWERS[this.knobs.toString()] || 'Unknown';
+  }
 
-  ngOnInit() {}
-
+  constructor() {}
 }
